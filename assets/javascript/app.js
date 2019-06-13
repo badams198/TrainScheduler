a40374f4e511926d40dd8c717237f5b08ecf189a
Badams <badams10@gmail.com>
2019-06-12 23:54:06 -0500

    added firebase code and title html

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//Add firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCJII16ZSJSy_GtwmhTCbypWkeo_iKsdok",
+    authDomain: "trainsch-9bfab.firebaseapp.com",
+    databaseURL: "https://trainsch-9bfab.firebaseio.com",
+    projectId: "trainsch-9bfab",
+    storageBucket: "trainsch-9bfab.appspot.com",
+    messagingSenderId: "278439584801",
+    appId: "1:278439584801:web:72ca2c90bcd11b18"
+  };
+
+  firebase.initializeApp(config);
